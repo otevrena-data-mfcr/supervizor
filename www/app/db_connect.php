19559c1b5db9f0259451128578a360dae1cb4ac3
Martin Kopeček <github@smallhill.cz>
2016-04-08 09:52:52 +0200

    Další oprava z lupa.cz

--- www/app/db_connect.php
+++ www/app/db_connect.php
@@ -5,13 +5,10 @@ function db_connect($host,$user,$pass){
   
   $driver_config = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"); 
   $pdo = new PDO("mysql:dbname=".PROFILE_DB.";host=$host",$user,$pass,$driver_config);
-  //$pdo = new PDO("mysql:dbname=zpevniky;host=127.0.0.1","root","",$driver_config);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  
-  //$cache_file_name = str_replace("/","_",$_SERVER["SCRIPT_NAME"]);
-  $cache = null;//new NotORM_Cache_Include(TMP_DIR."/notorm/notorm_cache_".$cache_file_name);
-  
-  $structure = null;//new NotORM_Structure_Discovery($pdo, $cache);
+    
+  $cache = null;
+  $structure = null;
   
   $db = new NotORM($pdo,$structure,$cache);
   
