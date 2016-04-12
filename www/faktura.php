d3df66c1f53e4d1252c6e0457c3769358013d9a4
Roman3349 <ondracek.roman@centrum.cz>
2016-04-12 07:22:43 +0200

    Načítání obsahu přes HTTPS, když to provozovatel služby podporuje

--- www/faktura.php
+++ www/faktura.php
@@ -123,7 +123,7 @@ $tpl->fetchOutput("styles");
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/cs_CZ/sdk.js#xfbml=1&version=v2.4";
+  js.src = "https://connect.facebook.net/cs_CZ/sdk.js#xfbml=1&version=v2.4";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
